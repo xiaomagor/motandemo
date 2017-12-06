f5d1c0be3e773cfbfece1c1d0e1b2fa4ce3569a3
xiaomagor <ma.ql@qq.com>
2017-12-06 12:49:48 +0800

    初始化

---
+++ rpc-motan-client/src/main/java/quickstart/Client.java
@@ -0,0 +1,18 @@
+package quickstart;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+
+
+public class Client {
+
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:motan_client.xml");
+        FooService service = (FooService) ctx.getBean("remoteService");
+
+        System.out.println(service.hello("motan"));
+    }
+}
